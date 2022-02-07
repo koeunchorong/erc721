929afb6d15291ba954c69d4e5819ca7cb95e7f4f
koeunchorong <koasis03@gmail.com>
2022-02-07 10:26:54 +0900

    erc721

---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,13 @@
+// const Migrations = artifacts.require("Migrations");
+
+// module.exports = function (deployer) {
+//   deployer.deploy(Migrations);
+// };
+
+const Migrations = artifacts.require("Migrations");
+const MyNFTs = artifacts.require("MyNFTs.sol"); // MyNFTs.sol 파일 추가
+
+module.exports = function (deployer) {
+  deployer.deploy(Migrations);
+  deployer.deploy(MyNFTs); // MyNFTs를 배포에 추가
+};
